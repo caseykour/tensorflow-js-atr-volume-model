05b058853acd845641ca3bf735248b5ced39e400
caseykour <36422415+caseykour@users.noreply.github.com>
2019-04-23 22:41:38 -0500

    Added test data set

--- src/model.js
+++ src/model.js
@@ -1,18 +1,22 @@
 'use strict'
 
+// Imports.
 const tf = require('@tensorflow/tfjs');
-const historicalPrices = require('./data/forex-aud-60min.data.json').results;
 const atrCalculator = require('../node_modules/quant-sdk/src/average-true-range.calculator');
+
+// Training and testing data sets.
+const trainingHistoricalPrices = require('./data/forex-aud-60min-training.data.json').results;
+const testingHistoricalPrices = require('./data/forex-aud-60min-testing.data.json').results;
 
 const MAX_EPOCH = 1500;
 const LEARNING_RATE = 0.1;
 
-function transformData(historicalPrices) {
-  let priceList = [...historicalPrices];
+function transformTrainingData(trainingHistoricalPrices) {
+  let priceList = [...trainingHistoricalPrices];
   let dataList = [];
 
-  // Map ATR and associated volume to new array 
-  for (let i = 0; i < historicalPrices.length - 14; i++) {
+  // Map ATR and associated volume to new array. 
+  for (let i = 0; i < trainingHistoricalPrices.length - 14; i++) {
     let averageTrueRange =
       atrCalculator.calculateAverageTrueRangePrice(
         priceList,
@@ -21,13 +25,13 @@ function transformData(historicalPrices) {
 
     dataList.push({
       averageTrueRange: averageTrueRange,
-      volume: historicalPrices[i].v
+      volume: trainingHistoricalPrices[i].v
     });
 
     priceList.pop();
   }
 
-  // Filter any outlier data (there is bad data in this set)
+  // Filter any outlier data (there is bad data in this set).
   return dataList.filter(x => x.averageTrueRange < 1);
 }
 
@@ -38,11 +42,13 @@ function run(xList, yList) {
   const W = tf.scalar(0.1).variable();
   const b = tf.scalar(0.1).variable();
 
-  // Define a linear regression model: f(x) = Wx + b
+  // Define a linear regression model: f(x) = Wx + b.
   const f = x => W.mul(x).add(b);
-  
-  // Define loss function
+
+  // Define loss function.
   const loss = (prediction, label) => prediction.sub(label).square().mean();
+
+  // Define optimizer strategy.
   const optimizer = tf.train.adam(LEARNING_RATE);
 
   // Train the model.
@@ -50,16 +56,15 @@ function run(xList, yList) {
     optimizer.minimize(() => loss(f(xs), ys));
   }
 
-  // Make predictions and compare with actual result.
-  const predictionList = f(xs).dataSync();
+  // Make predictions and print result.
+  const predictionList = f(testingHistoricalPrices.map(m => m.v)).dataSync();
   predictionList
     .forEach((p, i) => {
       console.log(`x: ${i}, prediction: ${p}`);
-      console.log(`x: ${i}, actual: ${yList[i]}`);
     });
 }
 
-let dataList = transformData(historicalPrices);
+let dataList = transformTrainingData(trainingHistoricalPrices);
 
 let xList = dataList.map(m => parseFloat(m.volume));
 let yList = dataList.map(m => parseFloat(m.averageTrueRange));
